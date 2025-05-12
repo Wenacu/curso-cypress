9ac60e00fa49e30ec8df078ed23c035dd7447419
Wenacu <wenacu98@gmail.com>
2025-05-12 11:56:56 -0600

    Login page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
+const { LoginMethods } = require("./pages/login.methods")
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login('username','password')
+    cy.wait(5000)
+
   })
 })
