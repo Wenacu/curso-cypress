27f9e234f5d83a83483747b2997681467d2e6d87
Wenacu <wenacu98@gmail.com>
2025-05-19 09:37:34 -0600

    Creacion de pagina Cart

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,4 @@
+const { CartMethods } = require("./pages/cart/cart.methods")
 const { HomeMethods } = require("./pages/home/home.methods")
 const { LoginMethods } = require("./pages/login/login.methods")
 
@@ -6,8 +7,10 @@ describe('template spec', () => {
     cy.visit('https://demoblaze.com/')
     //cy.get('a[data-target="#logInModal"]').click()
     //LoginMethods.login('username','password')
-    HomeMethods.clickOnProductLink('Iphone 6 32gb')
-    cy.wait(5000)
+    //HomeMethods.clickOnProductLink('Iphone 6 32gb')
+      cy.wait(30000)
+    CartMethods.clickOnDeleteLink('Nokia lumia 1520')
+    cy.wait(30000)
 
   })
 })
