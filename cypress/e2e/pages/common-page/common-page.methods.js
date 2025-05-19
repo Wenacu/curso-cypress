0b0a5d8049055e09b3d131d2dd48912d78c03924
Wenacu <wenacu98@gmail.com>
2025-05-19 11:48:27 -0600

    Creacion de pagina Common page

---
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -0,0 +1,26 @@
+import { CommonPageData } from "./common-page.data";
+import { CommonPageElements } from "./common-page.element";
+
+export class CommonPageMethods{
+static navigateToDemoBlaze(){
+    cy.visit(CommonPageData.url); 
+}
+static clickOnHomeOption(){
+    CommonPageElements.topMenu.home.click();
+}
+static clickOnContactOption(){
+    CommonPageElements.topMenu.contact.click();
+}
+static clickOnAboutUsOption(){
+    CommonPageElements.topMenu.aboutUs.click();
+}
+static clickOnCartOption(){
+    CommonPageElements.topMenu.cart.click();
+}
+static clickOnLoginOption(){
+    CommonPageElements.topMenu.login.click();
+}
+static clickOnSignOption(){
+    CommonPageElements.topMenu.signup.click();
+}
+}
